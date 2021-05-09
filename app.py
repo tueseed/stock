bb7d7ac4beb93a4a5557dd6f0c357489a28e2364
tueseed <tue_seed@hotmail.com>
2021-05-09 21:02:06 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -68,67 +68,16 @@ def reply(replyToken, databack):
     headers = {'Content-Type': 'application/json; charset=UTF-8','Authorization': LINE_API_KEY}
     # msgs = [{"type":"text","text":str(textList)}]
     flex = {
-            "type": "bubble",
-            "hero": {
-                        "type": "image",
-                        "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png",
-                        "size": "full",
-                        "aspectRatio": "20:13",
-                        "aspectMode": "cover",
-                        "action": {
-                                      "type": "uri",
-                                      "uri": "http://linecorp.com/"
-                                    }
-                    },
-            "body": {
-                        "type": "box",
-                        "layout": "vertical",
-                        "contents": [
-                                      {
-                                        "type": "text",
-                                        "text": "Brown Cafe",
-                                        "weight": "bold",
-                                        "size": "xl"
-                                      },
-                                      {
-                                        "type": "box",
-                                        "layout": "baseline",
-                                        "margin": "md",
-                                        "contents": [
-          {
-            "type": "icon",
-            "size": "sm",
-            "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
-          },
-          {
-            "type": "icon",
-            "size": "sm",
-            "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
-          },
-          {
-            "type": "icon",
-            "size": "sm",
-            "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
-          },
-          {
-            "type": "icon",
-            "size": "sm",
-            "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
-          },
-          {
-            "type": "icon",
-            "size": "sm",
-            "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gray_star_28.png"
-          },
-          {
-            "type": "text",
-            "text": "4.0",
-            "size": "sm",
-            "color": "#999999",
-            "margin": "md",
-            "flex": 0
-          }
-        ]
+  "type": "bubble",
+  "body": {
+    "type": "box",
+    "layout": "vertical",
+    "contents": [
+      {
+        "type": "text",
+        "text": "Brown Cafe",
+        "weight": "bold",
+        "size": "xl"
       },
       {
         "type": "box",
@@ -151,7 +100,6 @@ def reply(replyToken, databack):
               {
                 "type": "text",
                 "text": "Miraina Tower, 4-1-6 Shinjuku, Tokyo",
-                "wrap": "true",
                 "color": "#666666",
                 "size": "sm",
                 "flex": 5
@@ -173,7 +121,6 @@ def reply(replyToken, databack):
               {
                 "type": "text",
                 "text": "10:00 - 23:00",
-                "wrap": "true",
                 "color": "#666666",
                 "size": "sm",
                 "flex": 5
@@ -183,38 +130,6 @@ def reply(replyToken, databack):
         ]
       }
     ]
-  },
-  "footer": {
-    "type": "box",
-    "layout": "vertical",
-    "spacing": "sm",
-    "contents": [
-      {
-        "type": "button",
-        "style": "link",
-        "height": "sm",
-        "action": {
-          "type": "uri",
-          "label": "CALL",
-          "uri": "https://linecorp.com"
-        }
-      },
-      {
-        "type": "button",
-        "style": "link",
-        "height": "sm",
-        "action": {
-          "type": "uri",
-          "label": "WEBSITE",
-          "uri": "https://linecorp.com"
-        }
-      },
-      {
-        "type": "spacer",
-        "size": "sm"
-      }
-    ],
-    "flex": 0
   }
 }
     flexMsg =[{
